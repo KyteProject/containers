a9553f300191043485e7807c4d4636a87e663330
dan <dan@omux.dev>
2025-01-12 12:02:31 +0000

    Simplify chartDB

--- apps/chartdb/entrypoint.sh
+++ apps/chartdb/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-export PATH="/bin:/sbin:/usr/bin:/usr/sbin:${PATH}"
-
 # Replace the OpenAI API key in the built files if provided
 if [[ -n "${OPENAI_API_KEY}" ]]; then
     find /app/public -type f -name "*.js" -exec sed -i "s/VITE_OPENAI_API_KEY/${OPENAI_API_KEY}/g" {} +
 fi
 
-# Start the HTTP server
-exec /bin/busybox httpd -f -p 8080 -h /app/public
+# Start Python HTTP server
+exec python3 -m http.server 8080 --directory /app/public
